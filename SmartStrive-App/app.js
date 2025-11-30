734bb4cf4e52e0b61078339bb5446c851f960f53
tthpck <91bota@gmail.com>
2025-11-30 22:40:46 +0100

    301125

--- SmartStrive-App/app.js
+++ SmartStrive-App/app.js
@@ -7,7 +7,6 @@ const focusButton = document.querySelectorAll('.js-focus-button');
 const profileButton = document.querySelectorAll('.js-profile-button');
 
 
-
 //screens
 const allScreens = document.querySelectorAll('.screen');
 const homeScreen = document.querySelector('.js-home-screen');
@@ -15,10 +14,10 @@ const exploreScreen = document.querySelector('.js-explore-screen');
 const focusScreen = document.querySelector('.js-focus-screen');
 const profileScreen = document.querySelector('.js-profile-screen');
 const exploreScreenContent = document.querySelector('.js-explore-content');
-
-
-
-
+const subAreaScreen = document.querySelector('.js-subsections-screen');
+const subAreaContent = document.querySelector('.js-subsections-content');
+const actionsScreen = document.querySelector('.js-actions-screen');
+const actionsContent = document.querySelector('.js-actions-content');
 
 //event listeners buttons
 
@@ -68,15 +67,57 @@ function renderExploreScreen(){
   exploreScreenContent.innerHTML= 
     macroNames
       .map(name => 
-        `<button class = "js-macro-button macro-button"> ${name}</button>`)
+        `<button class = "js-macro-button macro-button" value = "${name}"> ${name}</button>`)
       .join ('');  
 
-      document.querySelectorAll('.js-macro-button').forEach(button =>
-        button.addEventListener('click', () =>{
-          console.log('ciao')   //DA RIVEDERE
+      document
+        .querySelectorAll('.js-macro-button')
+        .forEach(button =>
+          button.addEventListener('click', (event) =>{
+          renderSubArea(event.target.value)
       })
       )
 
 }
 
 
+//function sub-area buttons
+
+function renderSubArea(macroName) {
+    changeScreen(subAreaScreen)
+
+    const subArea = areaDatabase[macroName]
+
+    subAreaContent.innerHTML = subArea
+      .map(area => `<button class = "js-subarea-button subarea-button" value = "${area}">${area}</button>`)
+      .join('');
+
+
+     document
+      .querySelectorAll('.js-subarea-button')
+      .forEach(button => {
+        button.addEventListener('click', (event) => {
+          renderActions(macroName, event.target.value)
+        }) 
+      })
+}
+
+//function actions buttons
+
+function renderActions(macroName, subarea){
+  changeScreen(actionsScreen) 
+  
+  const actions = actionsDatabase[macroName][subarea]
+
+  actionsContent.innerHTML = actions
+    .map(a => `<ul>${a}</ul>`)
+    .join('')
+      //still to add delete buttons
+
+  
+  document.querySelector('.js-back-button')
+    .addEventListener('click', () => {
+      changeScreen(subAreaScreen)
+    })
+
+}
