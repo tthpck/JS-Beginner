d08169d90be23cd0794e932a8533c37e784086d4
tthpck <91bota@gmail.com>
2025-12-01 22:15:40 +0100

    0112252

--- SmartStrive-App/app.js
+++ SmartStrive-App/app.js
@@ -12,13 +12,13 @@ const allScreens = document.querySelectorAll('.screen');
 const homeScreen = document.querySelector('.js-home-screen');
 const exploreScreen = document.querySelector('.js-explore-screen');
 const focusScreen = document.querySelector('.js-focus-screen');
-const focusContent = document.querySelector('.js-focus-content');
+let focusContent = document.querySelector('.js-focus-content');
 const profileScreen = document.querySelector('.js-profile-screen');
-const exploreScreenContent = document.querySelector('.js-explore-content');
+let exploreScreenContent = document.querySelector('.js-explore-content');
 const subAreaScreen = document.querySelector('.js-subsections-screen');
-const subAreaContent = document.querySelector('.js-subsections-content');
+let subAreaContent = document.querySelector('.js-subsections-content');
 const actionsScreen = document.querySelector('.js-actions-screen');
-const actionsContent = document.querySelector('.js-actions-content');
+let actionsContent = document.querySelector('.js-actions-content');
 
 //p
 const welcomeMessage = document.querySelector('.js-welcome-message');
@@ -71,7 +71,7 @@ function renderUserName(){
 selectUserName.addEventListener('click', () => {
   const userNameChoice = inputUserName.value;
   localStorage.setItem('username', userNameChoice);
-  userName = localStorage.getItem('username');
+  userName = localStorage.getItem('user');
   renderUserName();
 });
 
@@ -91,67 +91,112 @@ const myFocus = JSON.parse(localStorage.getItem('focus')) || [];
 updateMyFocus()
 
 function updateMyFocus() {
-  if (myFocus.length === 0) {focusContent.innerHTML = 'Add a focus and start changing your life!'} 
-    else { focusContent.innerHTML = myFocus
-          .map(focus => `<p>${focus}</p> <button class = "js-delete-focus delete-focus">Remove</button>`)
-          .join('');
-  }
 
-  document.querySelectorAll('.js-delete-focus')
-    .forEach((button, index) => button.addEventListener('click', () => {
-      myFocus.splice(index, 1);
-      updateMyFocus();
-    }))
-}
+  focusContent.innerHTML = '';
+
+  if (myFocus.length !== 0) {
+  myFocus.forEach((focus,index) => {
+  const focusCard = document.createElement('div');
+  focusCard.classList.add('focus-div')
+
+  const focusText = document.createElement('p');
+  focusText.textContent = (focus)
+
+  const focusButton = document.createElement('button');
+   focusButton.textContent = ('Remove')
+   focusButton.classList.add('focus-remove-button')
+
+   const focusCheckBox = document.createElement('input');
+   focusCheckBox.type = ('checkbox');
+
+   focusCard.appendChild(focusCheckBox);
+   focusCard.appendChild(focusText);
+   focusCard.appendChild(focusButton);
+
+   focusContent.appendChild(focusCard);
 
 
 
+   focusButton.addEventListener('click', () =>{
+    myFocus.splice(index, 1);
+    localStorage.setItem('focus', JSON.stringify(myFocus))
+    updateMyFocus();
+   })
+})} else {
+
+  const placeHolderText = document.createElement('p');
+  placeHolderText.textContent = ('Add a focus and start changing your life!');
+
+  focusContent.appendChild(placeHolderText);
+}}
 
 
-
-//function explore screen
+//explore screen
 
 function renderExploreScreen(){
 
-  const macroNames = Object.keys(areaDatabase)
+  const macroNames = Object.keys(areaDatabase);
 
-  exploreScreenContent.innerHTML= 
-    macroNames
-      .map(name => 
-        `<button class = "js-macro-button macro-button" value = "${name}"> ${name}</button>`)
-      .join ('');  
+  exploreScreenContent.innerHTML = '';
 
-      document
-        .querySelectorAll('.js-macro-button')
-        .forEach(button =>
-          button.addEventListener('click', (event) =>{
-          renderSubArea(event.target.value)
-      })
-      )
+  macroNames.forEach(macroName => {
 
+  const macroCard = document.createElement('div');
+  macroCard.classList.add('macro-div');
+
+  const macroButton = document.createElement('button');
+  macroButton.classList.add('macro-button');
+  macroButton.value = (macroName)
+  macroButton.textContent = (macroName);
+
+
+  macroButton.addEventListener('click', (event) => {
+    renderSubArea(event.target.value)
+  }
+  )
+
+  macroCard.appendChild(macroButton);
+  exploreScreen.appendChild(macroCard)
+
+
+
+  })
 }
 
 
-//function sub-area buttons
+
+//function sub areas
 
 function renderSubArea(macroName) {
     changeScreen(subAreaScreen)
 
     const subArea = areaDatabase[macroName]
 
-    subAreaContent.innerHTML = subArea
-      .map(area => `<button class = "js-subarea-button subarea-button" value = "${area}">${area}</button>`)
-      .join('');
+subAreaContent.innerHTML = '';
 
+subArea.forEach(area => {
 
-     document
-      .querySelectorAll('.js-subarea-button')
-      .forEach(button => {
-        button.addEventListener('click', (event) => {
-          renderActions(macroName, event.target.value)
-        }) 
-      })
+const subAreaCard = document.createElement('div');
+subAreaCard.classList.add('subarea-div');
+
+const subAreaButton = document.createElement('button');
+subAreaButton.classList.add('subarea-button');
+subAreaButton.value = (area);
+subAreaButton.textContent = (area);
+
+subAreaButton.addEventListener('click', (event) => {
+  renderActions(macroName, event.target.value)
+})
+
+subAreaCard.appendChild(subAreaButton)
+subAreaContent.appendChild(subAreaCard);
+
+})
 }
+
+
+
+
 
 //function actions buttons
 
@@ -160,27 +205,38 @@ function renderActions(macroName, subarea){
   
   const actions = actionsDatabase[macroName][subarea]
 
-  actionsContent.innerHTML = actions
-    .map(action => `<ul>${action}</ul> <button class ="js-add-focus add-focus ">Add to My Focus</button>`)
-    .join('');
-  
-  document.querySelector('.js-back-button')
-    .addEventListener('click', () => {
-      changeScreen(subAreaScreen)
-    })
+actionsContent.innerHTML = '';
 
-  document.querySelectorAll('.js-add-focus')
-    .forEach((button, index) => 
-      button.addEventListener('click', () => {
-        if (myFocus.includes(actions[index])) return
+actions.forEach(actionText => {
+
+  const actionCard = document.createElement('div');
+  actionCard.classList.add('action-div');
+
+  const cardText = document.createElement('p');
+  cardText.textContent = (actionText)
+
+  const cardButton = document.createElement('button');
+  cardButton.textContent = ('Add to My Focus');
+
+  cardButton.addEventListener('click', () => {
+        if (myFocus.includes(actionText)) return
         else {
-        myFocus.push(actions[index]);
+        myFocus.push(actionText);
         localStorage.setItem('focus', JSON.stringify(myFocus));
         updateMyFocus();
         }
       })
-    )
+  
+  actionCard.appendChild(cardText);
+  actionCard.appendChild(cardButton);
+
+  actionsContent.appendChild(actionCard)
+})
+
 }
+
+
+
 
 
 
