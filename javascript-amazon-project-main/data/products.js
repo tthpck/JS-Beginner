cf9f217654ee0beab664770bcdb15ed6836d996c
tthpck <91bota@gmail.com>
2025-12-14 22:24:08 +0100

    backend products

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -72,6 +72,31 @@ class Appliance extends Product {
   }
 }
 
+
+export let products = [];
+
+
+
+export function loadproducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', ()=> {
+    products = JSON.parse(xhr.response).map(productDetails => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails)
+  } else if (productDetails.type === 'appliances') {
+    return new Appliance(productDetails)}
+   else {return new Product(productDetails)}});
+
+   
+   console.log('load products')
+   fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send()
+}
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -741,7 +766,7 @@ export const products = [
     return new Appliance(productDetails)}
    else {return new Product(productDetails)}});
 
-
+*/
 
 
   /*
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,10 +1,16 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-//import '../data/cart-class.js'
-import '../data/car.js';
-import '../data/backend-practice.js';
+import { loadproducts } from "../data/products.js";
 
-renderOrderSummary();
-renderPaymentSummary();
+//import '../data/cart-class.js'
+//import '../data/car.js';
+//import '../data/backend-practice.js';
+
+loadproducts(() =>{
+  renderOrderSummary();
+  renderPaymentSummary();
+})
+
+
 
 
