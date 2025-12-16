5efdc1874b7921b4e8711443053c51397476e90e
tthpck <91bota@gmail.com>
2025-12-16 08:06:20 +0100

    promises and promise.all

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,16 +1,56 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadproducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
 //import '../data/cart-class.js'
 //import '../data/car.js';
 //import '../data/backend-practice.js';
 
-loadproducts(() =>{
+
+Promise.all([
+  new Promise((resolve) => {
+  loadproducts( () => {
+    resolve('value1');
+    })
+  }),
+  new Promise((resolve) => {
+  loadCart(() => {
+    resolve();
+  });
+  }),
+]).then(()=> {
   renderOrderSummary();
   renderPaymentSummary();
 })
 
+/*
+new Promise((resolve) => {
+  loadproducts( () => {
+    resolve('value1');
+  })
 
+}).then((value) => {
+  console.log(value)
+  return new Promise((resolve) => {
+  loadCart(() => {
+    resolve();
+  });
+  })
+
+}).then (() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+})
+*/
+
+/*
+loadproducts(() =>{
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+})
+*/
 
 
